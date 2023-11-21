242fd4e7f8c4666e735a042ca00f0d34cfe9630b
hendra-iba <120019110+hendra-iba@users.noreply.github.com>
2023-11-21 23:04:09 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import os
 
 
 
-df=pd.read_csv(r'C:\Users\hendr\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\submission\dashboard\main.csv')
+df=pd.read_csv('main.csv')
 
 musim = {1: 'Semi', 2: 'Panas', 3: 'Gugur', 4: 'Dingin'}
 
